8752c814d9ffb5863f082563144d4f0f83436881
hategan <hategan@mcs.anl.gov>
2018-03-11 01:33:20 -0800

    Separate directory initializers

--- server/lib/DirectoryInitializer.py
+++ server/lib/DirectoryInitializer.py
@@ -1,20 +1,39 @@
 from wsgidav.middleware import BaseMiddleware
 import os
+from .PathMapper import HomePathMapper, TalePathMapper
 
 
 class DirectoryInitializer(BaseMiddleware):
-    def __init__(self, application, config):
+    def __init__(self, application, config, pathMapper):
         BaseMiddleware.__init__(self, application, config)
         self.application = application
         self.config = config
+        self.pathMapper = pathMapper
         self.initializedFor = {}
 
     def __call__(self, environ, start_response):
-        userName = environ['WT_DAV_AUTHORIZED_USER']
-        if userName not in self.initializedFor:
+        # subdir is the user/tale specific part of the directory
+        subdir = self.pathMapper.getSubdir(environ)
+        if subdir not in self.initializedFor:
             root = self.config['wt_home_dirs_root']
             if root is None:
                 raise EnvironmentError('wt_home_dirs_root not in config')
-            os.makedirs('/%s/%s' % (root, userName), exist_ok=True)
-            self.initializedFor[userName] = True
+            os.makedirs('/%s/%s' % (root, subdir), exist_ok=True)
+            self.initializedFor[subdir] = True
+        # use a multi-level path such that we don't end up with a large number of
+        # entries in a single directory.
+        # Specifically, use <firstLetterOfUsername>/<username> for homedir
+        # and <firstTwoLettersOfTaleId>/<taleId> for tales
+        # This means that we need to translate the logical path (e.g. /homes/<username>/<file>)
+        # to the concrete path (e.g. /homes/<username>[0]/username/<file>) at some point,
+        # and, since this is the last WT specific filter in the flow, as well as the
+        # first filter to be aware of the mapping, do it here
         return self.application(environ, start_response)
+
+class HomeDirectoryInitializer(DirectoryInitializer):
+    def __init__(self, application, config):
+        DirectoryInitializer.__init__(self, application, config, HomePathMapper())
+
+class TaleDirectoryInitializer(DirectoryInitializer):
+    def __init__(self, application, config):
+        DirectoryInitializer.__init__(self, application, config, TalePathMapper())
