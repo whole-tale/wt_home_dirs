91e650092ac11303ca7f14ead963475103b08723
hategan <hategan@mcs.anl.gov>
2018-03-19 13:10:03 -0700

    Let entries expire after a while (1s) in user/token cache and allow programmatic clearing of the cache

--- server/lib/WTDomainController.py
+++ server/lib/WTDomainController.py
@@ -1,6 +1,47 @@
 from girder.utility.model_importer import ModelImporter
 from girder.models.model_base import AccessException
 import datetime
+import time
+
+class CacheEntry:
+    def __init__(self, value):
+        self.value = value
+        self.ts = time.time()
+
+    def isExpired(self, delta):
+        return time.time() - self.ts > delta
+
+
+class Cache:
+    def __init__(self, expirationTime=1.0):
+        self.dict = {}
+        self.expirationTime = expirationTime
+
+    def get(self, key):
+        try:
+            entry = self.dict[key]
+        except KeyError:
+            return None
+        if entry.isExpired(self.expirationTime):
+            try:
+                del self.dict[key]
+            except KeyError:
+                pass
+            return None
+        else:
+            return entry.value
+
+    def set(self, key, value):
+        self.dict[key] = CacheEntry(value)
+
+    def remove(self, key):
+        try:
+            del self.dict[key]
+        except KeyError:
+            pass
+
+    def clear(self):
+        self.dict.clear()
 
 
 class WTDomainController(object):
@@ -9,11 +50,15 @@ class WTDomainController(object):
         self.userModel = ModelImporter.model('user')
         self.passwordModel = ModelImporter.model('password', 'wt_home_dir')
         self.tokenModel = ModelImporter.model('token')
-        self.tokenCache = {}
-        self.userCache = {}
+        self.tokenCache = Cache()
+        self.userCache = Cache()
 
     def __repr__(self):
         return self.__class__.__name__
+
+    def clearCache(self):
+        self.tokenCache.clear()
+        self.userCache.clear()
 
     def getDomainRealm(self, inputURL, environ):
         return self.realm
@@ -55,28 +100,28 @@ class WTDomainController(object):
         return True
 
     def _getUser(self, username):
-        if username in self.userCache:
-            return self.userCache[username]
+        user = self.userCache.get(username)
+        if user is not None:
+            return user
         user = self.userModel.findOne({'login': username})
         if user is None:
             return None
         else:
-            self.userCache[username] = user
+            self.userCache.set(username, user)
             return user
 
     def _getToken(self, tokenStr):
         now = datetime.datetime.utcnow()
-        if tokenStr in self.tokenCache:
-            token = self.tokenCache[tokenStr]
-        else:
+        token = self.tokenCache.get(tokenStr)
+        if token is None:
             token = self.tokenModel.load(tokenStr, force=True, objectId=False)
             if token is None:
                 return None
             else:
-                self.tokenCache[tokenStr] = token
+                self.tokenCache.set(tokenStr, token)
         if now > token['expires']:
             try:
-                del self.tokenCache[tokenStr]
+                self.tokenCache.remove[tokenStr]
             except KeyError:
                 pass
             return None
