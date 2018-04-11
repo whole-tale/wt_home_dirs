fe749d9978860df849ce7c2c89e5125d7a94099b
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2018-04-11 10:40:38 -0500

    exposePluginConfig has to be called before importing views
    Don't ask me why...

--- web_client/routes.js
+++ web_client/routes.js
@@ -1,16 +1,17 @@
+/* eslint-disable import/first */
+
 import router from 'girder/router';
 import events from 'girder/events';
-import exposePluginConfig from 'girder/utilities/PluginUtils';
-
-import ConfigView from './views/ConfigView';
-import PasswordManagementView from './views/PasswordManagementView';
+import { exposePluginConfig } from 'girder/utilities/PluginUtils';
 
 exposePluginConfig('wt_home_dir', 'plugins/wt_home_dir/config');
 
+import ConfigView from './views/ConfigView';
 router.route('plugins/wt_home_dir/config', 'WTHomeDirConfig', function () {
     events.trigger('g:navigateTo', ConfigView);
 });
 
+import PasswordManagementView from './views/PasswordManagementView';
 router.route('homedir/password', 'WTHomeDirPassword', function () {
     events.trigger('g:navigateTo', PasswordManagementView);
 });
